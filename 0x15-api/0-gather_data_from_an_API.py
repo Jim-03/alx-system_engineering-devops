8f5a3559bb80110b02ffe4ece426dc3cafc3edf6
Jim <jimmycn03@gmail.com>
2024-06-04 00:14:23 +0300

    Documenting the code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-""" module to return information from an api."""
+""" Module to return information from an api."""
 import sys
 import requests
 
 
 if __name__ == "__main__":
+    """ Gets an employee's informstion given an id."""
+    # The employee's id from the terminal
     employee_id = int(sys.argv[1])
+    # send a get request
     r = requests.get(
             f"https://jsonplaceholder.typicode.com/users/{employee_id}"
             )
+    # Capture the response in json format
     data = r.json()
+    # Get the employee's name from the json string
     employee_name = data['name']
+    # Find the employee's todo
     todo = requests.get(
             f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
             )
+    # COnvert the todo to json
     todo_data = todo.json()
+    # Find the tasks completed
     completed_tasks = 0
     for task in todo_data:
         if task['completed']:
